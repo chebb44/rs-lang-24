dee1e2618c3cbe3c36d3a034b788197edae35474
Andrey Baryshev <chebb@yandex.ru>
2020-06-17 18:41:43 +0400

    feat: add styles for header and sidebar

--- src/components/SidebarItemLinks/SidebarItemLinks.js
+++ src/components/SidebarItemLinks/SidebarItemLinks.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './SidebarItemLinks.scss';
 
 export const SidebarItemLinks = ({ items }) => {
   return items ? (
-    <ul className="">
+    <ul className="sidebar-sub-list-link">
       {items.map((item, index) => {
         return (
           <Link to={item.link} key={index}>
-            <li className="">{item.name}</li>
+            <button className="sub-links-btn btn">{item.name}</button>
           </Link>
         );
       })}
