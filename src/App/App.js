0ab00b9d6f3b4e463b5afa01aa7613c6d73dcc52
Andrey Baryshev <chebb@yandex.ru>
2020-06-18 10:44:47 +0400

    refactor: app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import { DictionaryPage } from './../pages/DictionaryPage/DictionaryPage';
 import { useSelector, useDispatch } from 'react-redux';
