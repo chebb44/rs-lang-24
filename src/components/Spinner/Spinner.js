d258e5ebbd0acf1c8eaebd1418266d8d9b645388
lugovczov <vova.lugovczov@mail.ru>
2020-06-19 00:25:32 +0300

    feat: add bg-image (london)

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Spinner.scss';
 import { Spin } from './assets/Spin.js';
+import london from '../../assets/img/england_PNG721.png';
 
 export const Spinner = () => {
   return (
     <div className="spinner">
       <Spin />
+      <img className="london-image" src={london} alt="london"></img>
     </div>
   );
 };
