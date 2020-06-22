fd5189c339177310fadff70f140e2fe0e9353e5d
Ihor Burenko <burenko@gmail.com>
2020-06-22 21:37:54 +0300

    feat: add markup for game page

--- src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
+++ src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
@@ -1,6 +1,39 @@
 import React from 'react';
-// import './SpeakItEnterScreen.scss';
+import './SpeakItGamePage.scss';
+import blank from './../../../../assets/img/blank.jpg';
 
 export const SpeakItGameScreen = function () {
-  return <div className="enter-screen">GameScreen</div>;
+  return (
+    <div className="speak-it__game-screen">
+      <div className="header">
+      </div>
+      <div className="game-score">
+      </div>
+      <div className="central-screen">
+        <div className="central-screen__image">
+          <img src={blank} alt="img"/>
+        </div>
+        <div className="translation__block">
+          <i className="material-icons md-36 icon__mic">mic</i>
+          <div className="central-screen__translation">
+            Перевод тут
+          </div>
+        </div>
+      </div>
+      <div className="cards">
+
+      </div>
+      <div className="buttons">
+        <button className="btn btn_speak-it btn_small button__reset active">
+          Сброс
+        </button>
+        <button className="btn btn_speak-it btn_wide button__speak-please">
+          SPEAK IT
+        </button>
+        <button className="btn btn_speak-it btn_small button__result">
+          Результаты
+        </button>
+      </div>
+    </div>
+  );
 };
