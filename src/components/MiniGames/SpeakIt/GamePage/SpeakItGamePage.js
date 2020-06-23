44c945d9c272f678678b6744647892e223fca524
Ihor Burenko <burenko@gmail.com>
2020-06-23 14:27:18 +0300

    feat: added audio to cards

--- src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
+++ src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './SpeakItGamePage.scss';
 import blank from './../../../../assets/img/blank.jpg';
+import { SpeakItCard } from '../SpeakItCard/SpeackItCard';
+
+const onClickCard = (event) => {
+  event.currentTarget.children[3] && event.currentTarget.children[3].play();
+};
 
 export const SpeakItGameScreen = function () {
   return (
@@ -11,7 +16,7 @@ export const SpeakItGameScreen = function () {
       </div>
       <div className="central-screen">
         <div className="central-screen__image">
-          <img src={blank} alt="img"/>
+          <img src={blank} alt="img" />
         </div>
         <div className="translation__block">
           <i className="material-icons md-36 icon__mic">mic</i>
@@ -21,7 +26,32 @@ export const SpeakItGameScreen = function () {
         </div>
       </div>
       <div className="cards">
-
+        <SpeakItCard
+          word={'tested'}
+          transcription={'transcription'}
+          audioSrc={'files/01_0006.mp3'}
+          onClickCard={onClickCard}
+        />
+        <SpeakItCard
+          word={'word'}
+          transcription={'transcription'}
+          audioSrc={'files/01_0004.mp3'}
+        />
+        <SpeakItCard
+          word={'word'}
+          transcription={'transcription'}
+          audioSrc={'files/01_0004.mp3'}
+        />
+        <SpeakItCard
+          word={'word'}
+          transcription={'transcription'}
+          audioSrc={'files/01_0004.mp3'}
+        />
+        <SpeakItCard
+          word={'word'}
+          transcription={'transcription'}
+          audioSrc={'files/01_0004.mp3'}
+        />
       </div>
       <div className="buttons">
         <button className="btn btn_speak-it btn_small button__reset active">
