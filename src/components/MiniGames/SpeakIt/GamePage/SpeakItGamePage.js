790b6514cc0b1dc552b612757f061b3c6c103dfe
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-06-23 16:55:04 +0300

    Add files via upload

--- src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
+++ src/components/MiniGames/SpeakIt/GamePage/SpeakItGamePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './SpeakItEnterScreen.scss';
+import './SpeakItEnterScreen.scss';
 
 export const SpeakItGameScreen = function () {
   return <div className="enter-screen">GameScreen</div>;
