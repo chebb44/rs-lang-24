bd4e9fbdbb5db932c02f663c79f40df0a4cb41ef
Ihor Burenko <burenko@gmail.com>
2020-06-24 13:51:00 +0300

    fix: some git changes

--- src/components/MiniGames/SpeakIt/SpeakItCard/SpeackItCard.js
+++ src/components/MiniGames/SpeakIt/SpeakItCard/SpeackItCard.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import './SpeackItCard.scss';
 import SpeakerImage from './SpeakerImage';
+import { FILES_URL } from '../../../../utilities/network/networkConstants';
 
-export const SpeakItCard = function ({
-  word,
-  transcription,
-  audioSrc,
-  onClickCard,
-}) {
-  const FILES_URL =
-    'https://raw.githubusercontent.com/irinainina/rslang-data/master/';
-
+export const SpeakItCard = function ({ cardData, onClickCard }) {
   return (
-    <div className="card" onClick={onClickCard}>
+    <div className="card" onClick={onClickCard} datacardid={cardData.id}>
       <span className="card__audio-icon">
         <SpeakerImage />
       </span>
-      <p className="card__word">{word}</p>
-      <p className="card__transcription">{transcription}</p>
-      <audio src={FILES_URL + audioSrc} />
+      <p className="card__word">{cardData.word}</p>
+      <p className="card__transcription">{cardData.transcription}</p>
+      <audio src={FILES_URL + cardData.audio} />
     </div>
   );
 };
