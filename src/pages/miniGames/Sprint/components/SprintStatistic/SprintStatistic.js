5bcde813b74e9081e6be2d83a921abfe4011128e
Andrey Baryshev <chebb@yandex.ru>
2020-06-27 13:53:13 +0400

    feat: statistic screen

--- src/pages/miniGames/Sprint/components/SprintStatistic/SprintStatistic.js
+++ src/pages/miniGames/Sprint/components/SprintStatistic/SprintStatistic.js
@@ -2,15 +2,35 @@ import React from 'react';
 import './SprintStatistic.scss';
 import { SprintStatisticItem } from './../SprintStatisticItem/SprintStatisticItem';
 import { UniversalButton } from './../../../../../components/UniversalButton/UniversalButton';
-export const SprintStatistic = ({ redirectToStartScreen }) => {
+export const SprintStatistic = ({ redirectToStartScreen, gameStat }) => {
+  console.log('SprintStatistic -> gameStat', gameStat);
+  const statArray = [];
+  for (const key in gameStat) {
+    if (gameStat.hasOwnProperty(key)) {
+      statArray.push({ date: key, data: gameStat[key] });
+    }
+  }
+  console.log('SprintStatistic -> statArray', statArray);
+
   return (
     <div className="sprint-statistic">
-      <UniversalButton
-        onClickHandler={redirectToStartScreen}
-        buttonText="Вернуться"
-        extraClasses="m-1 sprint-stat-btn"
-      />
-      <SprintStatisticItem />
+      <div className="sprint-stat-header">
+        <h3>Статистика</h3>
+        <UniversalButton
+          onClickHandler={redirectToStartScreen}
+          buttonText="Вернуться"
+          extraClasses="m-1 sprint-stat-close-btn"
+        />
+      </div>
+      <div className="sprint-stat-container">
+        {statArray.length > 0 ? (
+          statArray.map((statArray, i) => {
+            return <SprintStatisticItem key={i} dayStat={statArray} />;
+          })
+        ) : (
+          <h4>Статистика пока не собрана</h4>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/miniGames/Sprint/components/SprintStatisticItem/SprintStatisticItem.js
+++ src/pages/miniGames/Sprint/components/SprintStatisticItem/SprintStatisticItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './SprintStatisticItem.scss';
-export const SprintStatisticItem = () => {
-  return <div> Stat Item </div>;
+export const SprintStatisticItem = ({ dayStat: { date, data } }) => {
+  const bestResult = Math.max(...data);
+  const amountAttempts = data.length;
+  const title = `Результаты: ${data.join(', ')}`;
+  return (
+    <div className="sprint-day-stat" title={title}>
+      {`${date} вы играли ${amountAttempts} раз(а)! Лучший результат: ${bestResult} очков!`}
+    </div>
+  );
 };
