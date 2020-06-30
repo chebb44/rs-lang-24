2aa5859528d9af8caded6cc63c1398d08ebe8a49
Veronika Martinovich <veranika.mart@gmail.com>
2020-07-01 00:07:27 +0300

    fix: remove console log and change mock data

--- src/utilities/StatisticLineChart/groupChartDataForTooltip.js
+++ src/utilities/StatisticLineChart/groupChartDataForTooltip.js
@@ -9,6 +9,33 @@ export function groupChartDataForTooltip(/* learnedWords */) {
     '2020,6,28': 15,
     '2020,6,30': 1800,
     '2020,7,30': 157,
+    '2020,7,1': 157,
+    '2020,7,2': 157,
+    '2020,7,3': 157,
+    '2020,7,4': 157,
+    '2020,7,5': 157,
+    '2020,7,6': 157,
+    '2020,7,7': 157,
+    '2020,7,8': 157,
+    '2020,7,9': 157,
+    '2020,7,10': 157,
+    '2020,7,11': 157,
+    '2020,7,12': 157,
+    '2020,7,13': 157,
+    '2020,7,14': 157,
+    '2020,7,15': 157,
+    '2020,7,16': 157,
+    '2020,7,17': 157,
+    '2020,7,18': 157,
+    '2020,7,19': 157,
+    '2020,7,20': 157,
+    '2020,7,21': 157,
+    '2020,7,22': 157,
+    '2020,7,23': 157,
+    '2020,7,24': 157,
+    '2020,7,25': 157,
+    '2020,7,26': 157,
+    '2020,7,27': 157,
   };
 
   const learnedWordsArray = Object.entries(learnedWords);
@@ -18,8 +45,6 @@ export function groupChartDataForTooltip(/* learnedWords */) {
   }
 
   learnedWordsArray.forEach((item) => {
-    console.log(getDateByString(item[0]).getDate());
-
     const month = getDateByString(item[0]).getMonth();
     tooltipData[month].push(
       `${getDateByString(item[0]).getDate()} ${months[month]}: ${item[1]} слов`,
