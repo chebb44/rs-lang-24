c796aa39eb2f3b82388395434c5337ee8c1fae19
Ihor Burenko <burenko@gmail.com>
2020-06-30 21:27:55 +0300

    fix: add string to date function

--- src/pages/miniGames/Sprint/components/SprintStatisticItem/SprintStatisticItem.js
+++ src/pages/miniGames/Sprint/components/SprintStatisticItem/SprintStatisticItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './SprintStatisticItem.scss';
+import { getDateByString } from '../../../../../utilities/getDateStringByDate';
+
 export const SprintStatisticItem = ({ dayStat: { date, data } }) => {
   const bestResult = Math.max(...data);
   const amountAttempts = data.length;
   const title = `Результаты: ${data.join(', ')}`;
   return (
     <div className="sprint-day-stat" title={title}>
-      {`${new Date(
-        date,
-      ).toLocaleDateString()} вы играли ${amountAttempts} раз(а)! Лучший результат: ${bestResult} очков!`}
+      {`${getDateByString(date).toLocaleDateString()} 
+      вы играли ${amountAttempts} раз(а). Лучший результат: ${bestResult} очков.`}
     </div>
   );
 };
--- src/utilities/getDateStringByDate.js
+++ src/utilities/getDateStringByDate.js
@@ -6,3 +6,8 @@
 export const getDateStringByDate = (date) => {
   return `${date.getFullYear()},${date.getMonth()},${date.getDate()}`;
 };
+
+export const getDateByString = (string) => {
+  const [year, month, day] = string.split(',');
+  return new Date(year, month, day);
+};
