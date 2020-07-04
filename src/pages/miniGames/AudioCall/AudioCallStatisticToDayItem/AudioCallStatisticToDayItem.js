b668708a74113af7fff5c3fdb011d2662a41fda2
lugovczov <vova.lugovczov@mail.ru>
2020-07-04 18:57:02 +0300

    feat: update description

--- src/pages/miniGames/AudioCall/AudioCallStatisticToDayItem/AudioCallStatisticToDayItem.js
+++ src/pages/miniGames/AudioCall/AudioCallStatisticToDayItem/AudioCallStatisticToDayItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AudioCallStatisticToDayItem.scss';
+import { MAX_WORDS_FOR_GAME } from '../constants';
 export const AudioCallStatisticToDayItem = ({ dayStat: { date, data } }) => {
   const bestResult = Math.max(...data);
   const amountAttempts = data.length;
@@ -8,7 +9,7 @@ export const AudioCallStatisticToDayItem = ({ dayStat: { date, data } }) => {
     <div className="audio-call-statistic-to-day__item" title={title}>
       {`${new Date(
         +date,
-      ).toLocaleDateString()} вы играли ${amountAttempts} раз(а)! Лучший результат: ${bestResult} очков!`}
+      ).toLocaleDateString()} вы играли ${amountAttempts} раз(а)! Максимум ответов: ${bestResult}/${MAX_WORDS_FOR_GAME}`}
     </div>
   );
 };
