f645b48d0bd3498f11be68833ce6944c2c8cfb78
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-07-06 12:38:49 +0300

    fix: exit link

--- src/pages/miniGames/Savanna/SavannaStatisticPage/SavannaStatisticPage.js
+++ src/pages/miniGames/Savanna/SavannaStatisticPage/SavannaStatisticPage.js
@@ -1,20 +1,17 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { routes } from './../../../../App/constants/routes';
 export const SavannaStatistic = ({ answerNumber, questionNumber }) => {
   return (
     <div className="Statistic-page">
       <div className="table">
         вы ответили на : {answerNumber} из {questionNumber} вопросов
       </div>
-      <button
-        type="button"
-        className="btn btn-warning"
-        onClick={() => {
-          document.location.href = '/app';
-        }}
-      >
-        ОТЛИЧНО!
-      </button>
+      <Link to={routes.mainApp}>
+        <button type="button" className="btn btn-warning">
+          ОТЛИЧНО!
+        </button>
+      </Link>
     </div>
   );
 };
