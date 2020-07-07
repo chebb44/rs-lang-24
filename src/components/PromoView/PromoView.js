c37bb1c48b00c99494999bb7595675411d9a2dcd
Veronika Martinovich <veranika.mart@gmail.com>
2020-07-07 19:58:58 +0300

    feat: create promo page

--- src/components/PromoView/PromoView.js
+++ src/components/PromoView/PromoView.js
@@ -1,10 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PromoView.scss';
+import arrow from './assets/arrow-straight.png';
+import { whyUsItems } from './constants';
+import { WhyUsItem } from '../WhyUsItem/WhyUsItem';
 
 export const PromoView = function () {
+  const visibility = {
+    expanded: 'expanded',
+    collapsed: 'collapsed',
+  };
+  const [addInfoVisibility, setAddInfoVisibility] = useState(
+    visibility.collapsed,
+  );
+
+  const handleExpandButtonClick = () => {
+    if (addInfoVisibility === 'expanded') {
+      setAddInfoVisibility(visibility.collapsed);
+    } else {
+      setAddInfoVisibility(visibility.expanded);
+    }
+  };
+
   return (
-    <div className="promo__wrapper">
-      <h2>Promo page is here</h2>
+    <div className="promo">
+      <div className="promo__wrapper">
+        <div className="why-us">
+          <h2 className="heading">Почему RS Lang?</h2>
+          <div className="why-us__container">
+            {whyUsItems.map((item, index) => (
+              <WhyUsItem
+                key={index}
+                title={item.title}
+                description={item.description}
+                img={item.img}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="promo__wrapper">
+        <div className="interval-repeating">
+          <h2 className="heading">Методика интревального повторения</h2>
+          <p className="interval-repeating__heading">
+            Позволяет выучить большой объем информации за короткое время.
+          </p>
+          <h4 className="description">Как это работает?</h4>
+          <div className="interval-repeating__container">
+            <div className="interval-repeating__item">
+              <p>
+                Ты устанавливаешь количество новых и повторяемых слов в день,
+                режим обучения, а также индивидуальную сложность слов.
+              </p>
+              <button
+                className="interval-repeating__expand-button"
+                onClick={handleExpandButtonClick}
+              >
+                Смотреть режимы обучения
+              </button>
+              <ul
+                className={
+                  addInfoVisibility === 'expanded'
+                    ? 'learning-modes learning-modes_expanded'
+                    : 'learning-modes learning-modes_collapsed'
+                }
+              >
+                <li className="learning-modes__item">
+                  Только новые слова
+                  <p className="learning-modes__description">
+                    в обучении участвуют только новые, ранее не изученные слова
+                  </p>
+                </li>
+                <li className="learning-modes__item">
+                  Только изученные слова
+                  <p className="learning-modes__description">
+                    в обучении участвуют только ранее изученные слова
+                  </p>
+                </li>
+                <li className="learning-modes__item">
+                  Новые и изученные слова
+                  <p className="learning-modes__description">
+                    в обучении участвуют как ранее изученные, так и новые слова
+                    (соотношение новых и повторяемых слов можно выбрать в
+                    настройках)
+                  </p>
+                </li>
+                <li className="learning-modes__item">
+                  Только сложные слова
+                  <p className="learning-modes__description">
+                    в обучении участвуют слова, отмеченные как "сложные"
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <img
+              src={arrow}
+              alt="Arrow"
+              className="interval-repeating__arrow"
+            />
+            <div className="interval-repeating__item">
+              RS Lang создает для тебя индивидуальный урок на день в зависимости
+              от установленных предпочтений.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
