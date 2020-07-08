d136d5e602af494254116fd026a1ec7926024b61
lugovczov <vova.lugovczov@mail.ru>
2020-07-08 16:18:35 +0300

    feat: add loader for question img

--- src/pages/miniGames/Quiz/QuizQuestionImg/QuizQuestionImg.js
+++ src/pages/miniGames/Quiz/QuizQuestionImg/QuizQuestionImg.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './QuizQuestionImg.scss';
 import { FILES_URL } from '../../../../utilities/network/networkConstants';
+import { Loader } from './assets/Loader';
 
 export const QuizQuestionImg = ({ image }) => {
+  const [imgIsLoaded, setImgIsLoaded] = useState(true);
+
+  const img = new Image();
+  img.src = `${FILES_URL}${image}`;
+
+  img.onload = () => {
+    setImgIsLoaded(false);
+  };
   return (
     <div className="quiz-question-image">
-      <img
-        src={`${FILES_URL}${image}`}
-        alt="question"
-        width="100%"
-        height="100%"
-      />
+      {imgIsLoaded ? (
+        <Loader />
+      ) : (
+        <img
+          src={`${FILES_URL}${image}`}
+          alt="question"
+          width="100%"
+          height="100%"
+        />
+      )}
     </div>
   );
 };
