70fdb70dc35cc02adc7589be92064293a1cb3e1c
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-07-08 17:08:54 +0300

    fix: add savanna state

--- src/reducers/miniGamesStats/miniGamesStatsReducer.js
+++ src/reducers/miniGamesStats/miniGamesStatsReducer.js
@@ -2,14 +2,13 @@ import {
   SPRINT_SEND_GAME_RESULT,
   SET_ALL_MINI_GAMES_STATS,
   SPEAK_IT_SEND_GAME_RESULT,
-  AUDIO_CALL_SEND_GAME_RESULT,
-  RESET_ALL_MINI_GAMES_STATS,
+  SAVANNA_GET_GAME_RESULT,
 } from './miniGamesStatsActions';
 const defaultData = {
   miniGames: {
     sprint: {},
     speakIt: {},
-    audioCall: {},
+    savanna: {},
   },
 };
 
@@ -17,8 +16,6 @@ export const miniGamesStatsSelector = (state) => state.miniGamesStats;
 
 export const miniGamesStats = (state = defaultData, action) => {
   switch (action.type) {
-    case RESET_ALL_MINI_GAMES_STATS:
-      return defaultData;
     case SPRINT_SEND_GAME_RESULT:
       const { date, result } = action.payload;
       let dayStat = [];
@@ -55,36 +52,6 @@ export const miniGamesStats = (state = defaultData, action) => {
           },
         },
       };
-    case AUDIO_CALL_SEND_GAME_RESULT:
-      const { audioCallDate, audioCallResult } = action.payload;
-      let audioCallDayStat = [];
-      if (audioCallDate in state.miniGames.audioCall) {
-        audioCallDayStat = [
-          ...state.miniGames.audioCall[audioCallDate],
-          audioCallResult,
-        ];
-      } else {
-        audioCallDayStat = [audioCallResult];
-      }
-      return {
-        ...state,
-        miniGames: {
-          ...state.miniGames,
-          audioCall: {
-            ...state.miniGames.audioCall,
-            [audioCallDate]: audioCallDayStat,
-          },
-        },
-      };
-    case SET_ALL_MINI_GAMES_STATS: {
-      return {
-        ...state,
-        miniGames: {
-          ...state.miniGames,
-          ...action.payload.miniGames,
-        },
-      };
-    }
     case SAVANNA_GET_GAME_RESULT:
       const { SavannaDate, SavannaResults } = action.payload;
       let savannaDayStat = [];
@@ -106,6 +73,15 @@ export const miniGamesStats = (state = defaultData, action) => {
           },
         },
       };
+    case SET_ALL_MINI_GAMES_STATS: {
+      return {
+        ...state,
+        miniGames: {
+          ...state.miniGames,
+          ...action.payload.miniGames,
+        },
+      };
+    }
     default:
       return state;
   }
