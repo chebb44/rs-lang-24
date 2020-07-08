973d31a02222fbc718500ee6de8766ce58266efa
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-07-08 18:37:41 +0300

    Update getDateStringByDate.js

--- src/utilities/getDateStringByDate.js
+++ src/utilities/getDateStringByDate.js
@@ -1,7 +1,7 @@
-// export const getBeginDayTimeStamp = (date) => {
-//   date.setHours(0, 0, 0, 0);
-//   return Date.parse(date);
-// };
+export const getBeginDayTimeStamp = (date) => {
+  date.setHours(0, 0, 0, 0);
+  return Date.parse(date);
+};
 
 export const getDateStringByDate = (date) => {
   return `${date.getFullYear()},${date.getMonth()},${date.getDate()}`;
