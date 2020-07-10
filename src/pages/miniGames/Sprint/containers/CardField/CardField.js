586e88c443428a9343ba7c0fdee23d85eb6c0c2b
Ihor Burenko <burenko@gmail.com>
2020-07-10 17:27:31 +0300

    fix: linter

--- src/pages/miniGames/Sprint/containers/CardField/CardField.js
+++ src/pages/miniGames/Sprint/containers/CardField/CardField.js
@@ -10,7 +10,6 @@ import { actionMarkWord } from '../../../../../store/actionsForSaga';
 import { NEXT_TRAIN_WORD } from './../../../../../sagas/constants';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-
 export const CardField = ({
   cards,
   score,
